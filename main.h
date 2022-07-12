5d46c5638dbb478a10fed9e1261655e3dd472f3a
AloisBlue <aloismburu@gmail.com>
2022-07-12 12:52:48 -0700

    push customized function

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _strlen(char *str);
+void _puts(char *str);
+char *_convert(unsigned num, int base);
+
+#endif
